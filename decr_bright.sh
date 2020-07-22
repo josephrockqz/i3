74fdca84b223a56e67b1e2f7f28c11021c987b95
josephrockqz <63575165+josephrockqz@users.noreply.github.com>
2020-07-22 10:21:59 -0500

    Create decr_bright.sh

---
+++ decr_bright.sh
@@ -0,0 +1,17 @@
+current_brightness=$(xrandr --verbose | grep -m 1 -i brightness | cut -f2 -d ' ')
+
+decreaseBrightness()
+{
+	if awk -v x=$current_brightness 'BEGIN { exit (x > 0) ? 0 : 1 }'
+	then
+        	xrandr --output HDMI-0 --brightness $new_brightness
+        	xrandr --output DVI-I-1 --brightness $new_brightness
+        	exit 0
+	else
+        	exit 1
+	fi
+}
+
+new_brightness=`echo $current_brightness - 0.05 | bc`
+
+decreaseBrightness
